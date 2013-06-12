c7dda349ce2df9a0e0808e1d0f805f117133b1dc
Marco Martin <notmart@gmail.com>
2013-06-12 18:31:28 +0200

    basic channel editing

--- views/channelList.js
+++ views/channelList.js
@@ -1,79 +1,88 @@
 
     var win;
     var channelView;
-    var channelStore = Ext.create('Ext.data.TreeStore', {
-        autoLoad: true,
-        storeId:'channelStore',
-        fields:['id', 'name', 'description', 'image', 'active'],
-        folderSort: true,
-        root: 'channels',
-        proxy: {
-            type: 'ajax',
-            url: '/json/store/structure/',
-            reader: {
-                type: 'json',
-                root: 'channels'
-            },
-            writer: {
-                type: 'json',
-                writeAllFields: false,
-                root: 'channels'
-            }
-        }/*,
-        listeners: {
-            write: function(proxy, operation) {
-                console.log(operation);
-                if (operation.action == 'destroy') {
-                    main.child('#form').setActiveRecord(null);
-                }
-                Ext.example.msg(operation.action, operation.resultSet.message);
-            },
-            update: function(store, record, operation, eOpts ) {
-                record.raw.name = record.data.name;
-                record.raw.desc = record.data.desc;
-                record.raw.flatmarkup = record.data['markups.flat'];
-                record.raw.markup = record.data['markups.markup'];
-                record.raw.maxmarkup = record.data['markups.max'];
-                record.raw.minmarkup = record.data['markups.min'];
+    var cellEditing;
+    var storeId;
 
-                Ext.Ajax.request({
-                    url: '/json/store/update/' + record.raw.id,
-                    method: 'POST',
-                    params: $.param(record.raw),
-                    callback: function(response) {
-                        store.load();
-                    }
-                });
-            },
-            add: function(store, records, index, eOpts) {
-                console.log(records)
-                var record;
-                for (var i in records) {
-                    record = records[i];
-                    record.raw.name = record.data.name;
-                    record.raw.desc = record.data.desc;
-                    record.raw.flatmarkup = record.data['markups.flat'];
-                    record.raw.markup = record.data['markups.markup'];
-                    record.raw.maxmarkup = record.data['markups.max'];
-                    record.raw.minmarkup = record.data['markups.min'];
+    var channelStore;
 
-                    Ext.Ajax.request({
-                        url: '/json/store/create',
-                        method: 'POST',
-                        params: $.param(record.raw),
-                        callback: function(response) {
-                            store.load();
-                        }
-                    });
-                }
-            }
-        }*/
-        
-    });
+    function setStoreId(id) {
+        storeId = id
+        channelStore.proxy.url = '/json/store/structure/' + id;
+        channelStore.getRootNode().removeAll();
+        channelStore.load();
+    }
 
     function channelList(storeData) {
         var win;
         if (!win) {
+            channelStore = Ext.create('Ext.data.TreeStore', {
+                autoLoad: true,
+                storeId:'channelStore',
+                fields:['id', 'name', 'description', 'image', 'active'],
+                //folderSort: true,
+                root: 'channels',
+                clearOnLoad: true,
+                proxy: {
+                    type: 'ajax',
+                    url: '/json/store/structure/',
+                    reader: {
+                        type: 'json',
+                        root: 'channels'
+                    }
+                },
+                listeners: {
+                    update: function(store, record, operation, eOpts ) {
+                        //only go forward if eOpts is the name of a column
+                        if (!record.data.name || eOpts.length === 0 ||
+                            ['id', 'name', 'description', 'image', 'active'].indexOf(eOpts[0]) === -1) {
+                            return;
+                        }
+                        var data = {};
+                        record.raw.channel = {};
+                        record.raw.channel.name = record.data.name;
+                        record.raw.channel.desc = record.data.desc;
+                        record.raw.channel.image = record.data.image;
+                        record.raw.channel.parent = record.data.parentId;
+                        record.raw.channel.active = record.data.active;
+
+                        Ext.Ajax.request({
+                            url: '/json/store/channel/update/' + storeId + '/' + record.raw.id,
+                            method: 'POST',
+                            params: $.param(record.raw),
+                            callback: function(response) {
+                                store.load();
+                            }
+                        });
+                    },
+                    add: function(store, records, index, eOpts) {
+                        console.log(records)
+                        return;
+                        var record;
+                        for (var i in records) {
+                            record = records[i];
+                            record.raw.channel = {};
+                            record.raw.channel.name = record.data.name;
+                            record.raw.channel.desc = record.data.desc;
+                            record.raw.channel.image = record.data.image;
+                            record.raw.channel.active = record.data.active;
+
+                            Ext.Ajax.request({
+                                url: '/json/store/channel/create/' + storeId,
+                                method: 'POST',
+                                params: $.param(record.raw),
+                                callback: function(response) {
+                                    store.load();
+                                }
+                            });
+                        }
+                    }
+                }
+                
+            });
+            cellEditing = Ext.create('Ext.grid.plugin.CellEditing', {
+                clicksToEdit: 1
+            });
             channelView = Ext.create('Ext.tree.Panel', {
                 store: channelStore,
                 selType: 'checkboxmodel',
@@ -84,7 +93,8 @@
                     { header: 'Active', dataIndex: 'active', xtype: 'checkcolumn'}
                     ],
                 rootVisible: false,
-                region: 'center'
+                region: 'center',
+                plugins: [cellEditing]
             });
             win = Ext.create('widget.window', {
                 title: 'Channels of Store ' + storeData.name,
@@ -107,8 +117,8 @@
             });
         }
 
-        channelStore.proxy.url = '/json/store/structure/' + storeData.id;
-        channelStore.load();
+        setStoreId(storeData.id);
+
         if (win.isVisible()) {
             win.hide(this, function() {
                 button.dom.disabled = false;
