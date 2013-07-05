a2e2bdf7df72b27ca92ae1e5b2e880b48d538676
Marco Martin <notmart@gmail.com>
2013-07-05 23:47:21 +0200

    recreate the form every time
    we are messing too much with it (dynamically adding fields and renaming fields) to reliably reset its state every time

--- views/assetDetails.js
+++ views/assetDetails.js
@@ -6,7 +6,6 @@ var currentAsset;
 function loadAssetDetails(assetData) {
     currentAsset = assetData.id;
     var lastImageField = 0;
-    var assetDetailsForm;
 
     function addImageField() {
 
@@ -88,131 +87,126 @@ function loadAssetDetails(assetData) {
         ++lastImageField;
     }
 
-    if (!assetDetailsWindow) {
-        assetDetailsForm = Ext.create('Ext.form.Panel', {
-            //frame: true,
-            bodyPadding: 5,
-
-            fieldDefaults: {
-                labelAlign: 'right',
-                labelWidth: 90,
-                anchor: '100%'
-            },
-
-            overflowY: 'auto',
-
-            items: [{
-                id: 'name',
-                xtype: 'textfield',
-                name: 'info[name]',
-                fieldLabel: 'Name',
-                value: assetData.name
-            }, {
-                id: 'license',
-                xtype: 'numberfield',
-                name: 'info[license]',
-                fieldLabel: 'License (TODO: combobox)',
-                value: assetData.license,
-                minValue: 0,
-                maxValue: 200
-            }, {
-                id: 'version',
-                xtype: 'textfield',
-                name: 'info[version]',
-                fieldLabel: 'Version',
-                value: assetData.version
-            }, {
-                xtype: 'filefield',
-                name: 'file[0]',
-                fieldLabel: 'File (TODO)'
-            }, {
-                id: 'description',
-                xtype: 'textareafield',
-                name: 'info[description]',
-                fieldLabel: 'Description',
-                value: assetData.description
-            }, {
-                id: 'baseprice',
-                xtype: 'numberfield',
-                name: 'info[baseprice]',
-                fieldLabel: 'Base Price',
-                value: assetData.baseprice,
-                minValue: 0,
-                maxValue: 200
-            }, {
-                id: 'active',
-                xtype: 'checkboxfield',
-                name: 'info[active]',
-                fieldLabel: 'Active',
-                value: assetData.active
-            }, {
-                id: 'posted',
-                xtype: 'hidden',
-                name: 'info[posted]',
-                value: false
-            }, {
-                id: 'id',
-                xtype: 'hidden',
-                name: 'info[id]',
-                value: assetData.id
-            }],
-
-            buttons: [{
-                text: 'Save',
-                handler: function() {
-
-                    var form = this.up('form').getForm();
-                    if (form.isValid()) {
-                        //disable the last file field, since is empty and with invalid data
-                        assetDetailsForm.items.get('file[' + (lastImageField-1) + ']').disable();
-                        assetDetailsForm.items.get('info[previews][' + (lastImageField-1) + '][type]').disable();
-                        assetDetailsForm.items.get('info[previews][' + (lastImageField-1) + '][subtype]').disable();
-
-                        form.submit({
-                            url: '/json/asset/update/' + currentAsset,
-                            waitMsg: 'Updating the asset...',
-                            //params: $.param({info: data}),
-                            success: function(fp, o) {
-                                store.removeAll();
-                                store.load();
-                                assetDetailsWindow.hide();
-                            },
-                            failure: function(form, action) {
-                                assetDetailsWindow.hide();
-                            }
-                        });
-                    }
-                }
-            },{
-                text: 'Cancel',
-                handler: function() {
-                    assetDetailsWindow.hide();
-                }
-            }]
-        });
-        addImageField();
-
-
-        assetDetailsWindow = Ext.create('widget.window', {
-            title: 'Details of Assets ' + assetData.name,
-            closable: true,
-            closeAction: 'hide',
-            modal: true,
-            width: '80%',
-            height: '80%',
-            layout: 'fit',
-            bodyStyle: 'padding: 5px;',
-            items: [assetDetailsForm]
-        });
-    } else {
-        assetDetailsForm.items.get('name').setValue(assetData.name);
-        assetDetailsForm.items.get('license').setValue(assetData.license);
-        assetDetailsForm.items.get('version').setValue(assetData.version);
-        assetDetailsForm.items.get('description').setValue(assetData.description);
-        assetDetailsForm.items.get('baseprice').setValue(assetData.baseprice);
-        assetDetailsForm.items.get('active').setValue(assetData.active);
-        assetDetailsForm.items.get('id').setValue(assetData.id);
+    if (assetDetailsWindow) {
+        assetDetailsWindow.destroy();
+        assetDetailsForm.destroy();
     }
+    assetDetailsForm = Ext.create('Ext.form.Panel', {
+        //frame: true,
+        bodyPadding: 5,
+
+        fieldDefaults: {
+            labelAlign: 'right',
+            labelWidth: 90,
+            anchor: '100%'
+        },
+
+        overflowY: 'auto',
+
+        items: [{
+            id: 'name',
+            xtype: 'textfield',
+            name: 'info[name]',
+            fieldLabel: 'Name',
+            value: assetData.name
+        }, {
+            id: 'license',
+            xtype: 'numberfield',
+            name: 'info[license]',
+            fieldLabel: 'License (TODO: combobox)',
+            value: assetData.license,
+            minValue: 0,
+            maxValue: 200
+        }, {
+            id: 'version',
+            xtype: 'textfield',
+            name: 'info[version]',
+            fieldLabel: 'Version',
+            value: assetData.version
+        }, {
+            xtype: 'filefield',
+            name: 'file[0]',
+            fieldLabel: 'File (TODO)'
+        }, {
+            id: 'description',
+            xtype: 'textareafield',
+            name: 'info[description]',
+            fieldLabel: 'Description',
+            value: assetData.description
+        }, {
+            id: 'baseprice',
+            xtype: 'numberfield',
+            name: 'info[baseprice]',
+            fieldLabel: 'Base Price',
+            value: assetData.baseprice,
+            minValue: 0,
+            maxValue: 200
+        }, {
+            id: 'active',
+            xtype: 'checkboxfield',
+            name: 'info[active]',
+            fieldLabel: 'Active',
+            value: assetData.active
+        }, {
+            id: 'posted',
+            xtype: 'hidden',
+            name: 'info[posted]',
+            value: false
+        }, {
+            id: 'id',
+            xtype: 'hidden',
+            name: 'info[id]',
+            value: assetData.id
+        }],
+
+        buttons: [{
+            text: 'Save',
+            handler: function() {
+
+                var form = this.up('form').getForm();
+                if (form.isValid()) {
+                    //disable the last file field, since is empty and with invalid data
+                    assetDetailsForm.items.get('file[' + (lastImageField-1) + ']').disable();
+                    assetDetailsForm.items.get('info[previews][' + (lastImageField-1) + '][type]').disable();
+                    assetDetailsForm.items.get('info[previews][' + (lastImageField-1) + '][subtype]').disable();
+
+                    form.submit({
+                        url: '/json/asset/update/' + currentAsset,
+                        waitMsg: 'Updating the asset...',
+                        //params: $.param({info: data}),
+                        success: function(fp, o) {
+                            store.removeAll();
+                            store.load();
+                            assetDetailsWindow.hide();
+                        },
+                        failure: function(form, action) {
+                            assetDetailsWindow.hide();
+                        }
+                    });
+                }
+            }
+        },{
+            text: 'Cancel',
+            handler: function() {
+                assetDetailsWindow.hide();
+            }
+        }]
+    });
+    addImageField();
+
+
+    assetDetailsWindow = Ext.create('widget.window', {
+        title: 'Details of Assets ' + assetData.name,
+        closable: true,
+        closeAction: 'hide',
+        modal: true,
+        width: '80%',
+        height: '80%',
+        layout: 'fit',
+        bodyStyle: 'padding: 5px;',
+        items: [assetDetailsForm]
+    });
+
 
     if (assetDetailsWindow.isVisible()) {
         assetDetailsWindow.hide();
