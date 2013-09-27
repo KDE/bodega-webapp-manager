2e8721d72cadd46b582dacd3f9de858f6df5c9b3
Aaron Seigo <aseigo@kde.org>
2013-09-27 16:44:43 +0200

    take an optional 'production' that makes it use forever

--- main.sh
+++ main.sh
@@ -1,2 +1,5 @@
-#NODE_ENV=production node app.js
-NODE_ENV=test node app.js
+#!/bin/sh
+case "$1" in
+    "production" ) forever start -a -l bodega-manager-production.log -o bodega-manager-production-out.log -e bodega-manager-production-error.log app.js;;
+               * ) node app.js;;
+esac
