08e2a0945cbe33c3f427b4e2adaf0af539fa0ce3
Aaron Seigo <aseigo@kde.org>
2013-11-08 11:12:20 +0100

    add a staging directive

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 case "$1" in
     "production" ) forever start -a -l bodega-manager-production.log -o bodega-manager-production-out.log -e bodega-manager-production-error.log app.js;;
-               * ) node app.js;;
+    "staging" ) forever start -a -l bodega-manager-test.log -o bodega-manager-test-out.log -e bodega-manager-test-error.log app.js;;
+    * ) node app.js;;
 esac
